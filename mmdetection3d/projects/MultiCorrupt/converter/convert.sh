9630925dfc9411d5c92abd1ba0bc6795cc1facdc
Jason Wu <jasonw02020@gmail.com>
2026-02-03 11:00:26 -0800

    Minor changes

--- mmdetection3d/projects/MultiCorrupt/converter/convert.sh
+++ mmdetection3d/projects/MultiCorrupt/converter/convert.sh
@@ -3,20 +3,31 @@
 
 cd /workspace/mmdetection3d/projects/MultiCorrupt/converter
 
-
-python img_converter.py \
---corruption dark \
+python lidar_converter.py \
+--corruption fog \
 --root_folder /workspace/mmdetection3d/data/nuscenes \
---dst_folder /workspace/mmdetection3d/data/multicorrupt/dark/3 \
+--dst_folder /workspace/mmdetection3d/data/multicorrupt/fog/3 \
 --severity 3 \
---n_cpus 8
+--n_cpus 31 \
+--sweep True
 
 python lidar_converter.py \
---corruption beamsreducing \
+--corruption snow \
 --root_folder /workspace/mmdetection3d/data/nuscenes \
---dst_folder /workspace/mmdetection3d/data/multicorrupt/test/3 \
+--dst_folder /workspace/mmdetection3d/data/multicorrupt/snow/3 \
 --severity 3 \
---n_cpus 8 \
+--n_cpus 31 \
 --sweep True
+
+python img_converter.py \
+--corruption snow \
+--root_folder /workspace/mmdetection3d/data/nuscenes \
+--dst_folder /workspace/mmdetection3d/data/multicorrupt/snow/3 \
+--severity 3 \
+--n_cpus 31
+
+
+
+
 
 
